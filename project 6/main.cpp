f8a7da57f8ff19b3e7e4b58cd1ececf5a19319ad
minavadamee20 <34077203+minavadamee20@users.noreply.github.com>
2018-10-17 13:16:04 -0700

    Update project 6/main.cpp

--- project 6/main.cpp
+++ project 6/main.cpp
@@ -9,3 +9,19 @@ int main() {
 	system("pause");
 	return 0;
 }
+
+
+/*OUTPUT
+
+total amount of shirts: [ 355 ].
+total amount of large shirts: [ 110 ].
+total black shirts: [ 90 ].
+
+
+total number of all shirts: [ 171 ]
+total number of medium shirts: [ 57 ]
+total number of short sleeves: [ 15 ]
+total number of red shirts: [ 24 ]
+Press any key to continue . . .
+
+*/
